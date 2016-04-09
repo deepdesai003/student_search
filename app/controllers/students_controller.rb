9bf11ce58c67132300151fec59b90c2a83bac170
CST680 Student <deepdesai003@gmail.com>
2016-04-09 01:16:37 -0400

    make it better

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,14 +2,18 @@ class StudentsController < ApplicationController
 def create
   @student = Student.new(params.require(:student).permit(:student_name, :e_no, :col, :major))
  
-  @student.save
+  if @student.save
   redirect_to @student
+  else 
+     render 'new'
+end
 end
  
   def show
     @student = Student.find(params[:id])
   end
 def new
+ @student = Student.new
 end
 def index
 @student= Student.all
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ActiveRecord::Base
+validates :student_name, presence: true
+validates :e_no, presence: true,
+                    length: { minimum: 6 }
+validates :col, presence: true
+validates :major, presence: true
 end
