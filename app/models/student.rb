9ae13e577fafeb2fb4582e6687b5450e98926316
CST680 Student <deepdesai003@gmail.com>
2016-04-09 21:04:17 -0400

    make it bettercd

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,13 @@
+require 'elasticsearch/model'
 class Student < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
 validates :student_name, presence: true
 validates :e_no, presence: true,
                     length: { minimum: 6 }
 validates :col, presence: true
 validates :major, presence: true
 end
+Student.import
+# for auto sync model with elastic search
