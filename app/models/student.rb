9c28301c6ec826f5bd45f781d2a9b1046c2a57d2
CST680 Student <deepdesai003@gmail.com>
2016-04-11 01:51:19 -0400

    make it better

--- app/models/student.rb
+++ app/models/student.rb
@@ -9,9 +9,6 @@ validates :e_no, presence: true,
 validates :col, presence: true
 validates :major, presence: true
 end
-<<<<<<< HEAD
+
 Student.import force: true
-=======
-Student.import
->>>>>>> db8426fef5cba11bd092e7137b950c8e815db2d3
 # for auto sync model with elastic search
